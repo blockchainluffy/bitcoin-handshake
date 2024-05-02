1d94f442085d954cb3f608f4e7fae0c28cd0157f
blockchainluffy <jainpunit1997@gmail.com>
2024-05-02 19:07:36 +0530

    feat: implemented handshake for bitcoin node

---
+++ test/tcp_transport_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"bitcoin-handshake/handshake"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTCPTransport(t *testing.T) {
+	opts := handshake.TCPTransportOpts{
+		ListenAddr:    ":3000",
+		HandshakeFunc: handshake.Handshake,
+	}
+	tr := handshake.NewTCPTransport(opts)
+	assert.Equal(t, tr.ListenAddr, ":3000")
+
+	assert.Nil(t, tr.ListenAndAccept())
+}
